03ab4d32bbaa178cd8bc0c447dc5023499f797e1
Makonya <makonya2302@gmail.com>
2020-05-20 14:25:20 +0600

    Lesson10

--- src/com/java/Runner.java
+++ src/com/java/Runner.java
@@ -1,57 +1,65 @@
 package com.java;
 
 public class Runner {
-    private final String ERROR_MESSAGE;
-    private static final int COUNT;
-    private Inner inner;
-
-    public void createInner(){
-        inner = new Inner();
-    }
-
-    {
-        ERROR_MESSAGE = "This is error";
-    }
-
-    static {
-        COUNT = 34;
-    }
-    /**
-     * Вложенный, статический класс
-     * */
-    static class Main{
-
-    }
-
-    /**
-     * Внутренний класс
-     * */
-    private class Inner{
-
-    }
-
     public static void main(String[] args) {
-        Runner.Main main = new Runner.Main();
+        /**
+         * Объвление переменной - указание типа переменной, названия
+         * */
+        int val;
 
-        for (Month value : Month.values()) {
-            System.out.println(value);
-            System.out.println("Name ru: " + value.getNameRu());
-            System.out.println("Number of month: " + value.getCount());
-            System.out.println("Season: " + value.getSeason());
+        /**
+         * Инициализация переменной - задание значения переменной
+         * */
+        val = 0;
+        val++;
+        System.out.println(Season.WINTER);
+        if (true) {
+            int temp = 5;
+            System.out.println(temp);
+        }
+        int mainMethodVariable = 8;
+        doSmt(mainMethodVariable);
+
+        /**
+         * break - выходит с цикла
+         * */
+        int n = 1;
+        for (int i = 0; i < 10; i++) {
+            if (n > 12) {
+                break;
+            } else {
+                n = n * 2;
+            }
+        }
+        /**
+         * continue - переходит на следующую итерацию цикла
+         * % - остаток от деления
+         * */
+        for (int i = 0; i < 10; i++) {
+            if (i%2==0){
+                continue;
+            }
+            System.out.println(i);
         }
 
         /**
-         * static - модификатор
-         * 1) Статический импорт - импортируем статический данные с другого класса
-         * import static com.java.Season.WINTER;
-         *
-         * 2) Статическая переменная - позволяет использовать переменные без создания объкета
-         *
-         * 3) Статический метод - позволяет использовать методы без создания объкета
-         *
-         * 4) Статичкий блок инициализации
-         *
-         * 5) Статический класс - вложенный класс
+         * Обработка исключений
          * */
+        try{
+            int result = 2/0;
+        } catch (ArithmeticException ex){
+            ex.printStackTrace();
+        }
+
+        System.out.println("Bye");
     }
+
+    private static void doSmt(int val) {
+        val++;
+    }
+}
+
+enum Season {
+    WINTER,
+    SUMMER
 }
